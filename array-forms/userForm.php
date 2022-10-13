c9f5943e9b554ba2dfe899e67e7904d4104ecb39
Andre Chirindza <andrechirindza@gmail.com>
2022-10-13 12:13:44 +0200

    Adding country

--- array-forms/userForm.php
+++ array-forms/userForm.php
@@ -8,13 +8,20 @@
 </head>
 <body>
     <div>
-        <label for="name">Name</label> <br>
-        <input type="text" name="name" id="name" placeholder="Enter your name"> <br>
-        <input type="email" name="email" id="email" placeholder="Enter your email"> <br>
-        <input type="text" name="gender" id="gender" placeholder="Enter your gender"> <br>
-        <input type="date" name="birthday" id="birthday"><br>
-
-        <input type="submit" value="Submit">
+        <form action="user_data.php" method="post">
+            <label for="name">Name</label> <br>
+            <input type="text" name="name" id="name" placeholder="Enter your name"> <br>
+            <label for="email">Email</label> <br>
+            <input type="email" name="email" id="email" placeholder="Enter your email"> <br>
+            <label for="gender">Gender</label> <br>
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender"> <br>
+            <label for="birthday">Date of birth</label> <br>
+            <input type="date" name="birthday" id="birthday"><br>
+            <label for="birthday">Country</label> <br>
+            <input type="text" name="country" id="country"><br>
+    
+            <input type="submit" value="Submit">
+        </form>
     </div>
 </body>
 </html>
